11fd2c09c5149cf841fa0319e7ec885941a0946e
Hailing Fang <benjaminfang.ol@outlook.com>
2024-05-11 14:49:25 +0800

    change to book_theme

--- conf.py
+++ conf.py
@@ -9,7 +9,7 @@
 project = "Benjamin Fang's Docs"
 copyright = '2023-2024, Benjamin Fang'
 author = 'Benjamin Fang'
-release = '0.1'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -26,11 +26,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst', 'file-form
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #using rtd theme and modify it's settings
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 html_theme_options = {
-    "analytics_id" : None,
-    "logo_only" : True,
-    "display_version" : True
 }
 
 #change the logo
