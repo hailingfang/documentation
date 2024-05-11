40c2db49ffad96fabc693fe99e40a34d2ad1e066
Hailing Fang <benjaminfang.ol@outlook.com>
2024-05-11 16:55:27 +0800

    config

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ release = '0.2.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser", "sphinx_rtd_theme", "sphinx.ext.mathjax"]
+extensions = ["myst_parser", "sphinx_book_theme", "sphinx.ext.mathjax"]
 
 templates_path = ['_templates']
 html_static_path = ['_static']
@@ -28,8 +28,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst', 'file-form
 #using rtd theme and modify it's settings
 html_theme = 'sphinx_book_theme'
 html_theme_options = {
+    "repository_url": "https://github.com/benjaminfang/documentation",
+    "use_repository_button": True,
+    'analytics_id': 'G-VWP9HESP22'
 }
 
 #change the logo
 github_url = "https://github.com/benjaminfang/documentation"
 html_logo = "img/doc-logo.svg"
+html_title = "My site title"
